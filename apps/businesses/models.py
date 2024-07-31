8be13e5610d2006726952f520171f5a9e527e43b
deaniway <ad.way@mail.ru>
2024-07-31 15:26:57 +0400

    add fields worker

--- apps/businesses/models.py
+++ apps/businesses/models.py
@@ -4,11 +4,16 @@ from django.contrib.auth import get_user_model
 
 class Business(models.Model):
     name = models.CharField(max_length=64)
+    description = models.TextField(
+        max_length=100,
+        null=True,
+        help_text="В случае нескольких предприятий одного бренда"
+    )
 
 
 class BusinessToUser(models.Model):
-    user = models.ForeignKey( get_user_model(), on_delete=models.PROTECT )
-    business = models.ForeignKey( Business, on_delete=models.PROTECT )
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    business = models.ForeignKey(Business, on_delete=models.PROTECT)
 
     OWNER = 'owner'
     READONLY = 'readonly'
